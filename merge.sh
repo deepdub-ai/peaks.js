fd83dcdefb093987c129bebbbfbacaadd4de0977
dutzi <dutzi.b@gmail.com>
2021-02-22 00:47:58 +0200

    fix max zoom

---
+++ merge.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+# echo "1" && git cherry-pick 4fd0513f8ac40c540e4038a33712b561fb98c5e0
+# echo "2" && git cherry-pick b91956a444acaa786ae41faba22c5c406dc87e88
+# echo "3" && git cherry-pick daedfcc125dd5f3ecadf3a4f77922c5aa07ccb64
+# echo "4" && git cherry-pick 3932118fa159d24864b62ae563cdd3add291c2d2
+# echo "5" && git cherry-pick 3f443a06b92df78bd986c886d7212b594d53a82b
+# echo "6" && git cherry-pick 0058db5f80cd3f4d6ce1a54c6412503a35fb7abc
+# echo "7" && git cherry-pick 8e5726758e8798735f91329389d71749d75fcc30
+# echo "8" && git cherry-pick f970fd9704fb2302d1f8a393ad0965aaf64c676b
+# echo "9" && git cherry-pick 20be20fa8bdf89a4f32b2fbb9713aac560d6c9de
+# echo "10" && git cherry-pick c92aa97f7bee0b6bf22011080d36ad166e0cc335
+# echo "11" && git cherry-pick 5411e5a657b41b654e1ba8ea6bb33f6e6c1b62
+# echo "12" && git cherry-pick 55c3499d4f1a86d4d0ccf7fb9f32d1fb34bcd4cf
+# echo "13" && git cherry-pick 5c70f963eb5abc6208b182a03ddeac4089362a55
+# echo "14" && git cherry-pick c85704ed07649e33eabc6e6509ae3e7ee17e1a9c
+# echo "15" && git cherry-pick 5de9539b1422bd675f42bc4ed5d6d26fa656b203
+# echo "16" && git cherry-pick 15b055e0fa061398a221fffd0481d29806f07f29
+# echo "17" && git cherry-pick 716bfef8092707f59dac5625f9366c301f9db121
+# echo "18" && git cherry-pick 9e6d815b9978c0b7685cdd78ec86958c88ea2644
+# echo "19" && git cherry-pick 0c9cb3ddc11d76e6966380a047d938819e7b724c
+# echo "20" && git cherry-pick aa2dc8d0e8f0fc847f69590668d7a9ee3d02ee64
+# echo "21" && git cherry-pick 768e2a230621ce713a3a8befefac641388d9b123
+# echo "22" && git cherry-pick d7719894bb2049a82b577a299b164875e09702a6
+# echo "23" && git cherry-pick 41f1c16a618d55ed56137e35a7c4cfefd85256f8
+# echo "24" && git cherry-pick 6076dddf33d282e194b885ee46e9126459073d51
+# echo "25" && git cherry-pick 564e0001037102fdb3f72821a4e317427f360812
+# echo "26" && git cherry-pick d188f7228235e2789dc737d74c41bb80b9661ea7
+# echo "27" && git cherry-pick 0126dfd2b9a52f072987730f792e18f24e93700e
+# echo "28" && git cherry-pick c87d7570059b3dc34e82b967aa18cb645813cfca
+# echo "29" && git cherry-pick 47d112a9f9760a32632f8441cfa9a8121e078670
+# echo "30" && git cherry-pick c0a9af66ffb216787c443de772b16ef9747d3e06
+# echo "31" && git cherry-pick f2c5522d917007a96995b3299aec96678343e048
+echo "32" && git cherry-pick 6c88fe73f51fa50f98948b57c4e6d434de0d69a5
+echo "33" && git cherry-pick c6a7c8a725a7629ce15e23ff23464844643d4043
+echo "34" && git cherry-pick 95c487671effc1b1f619f84292e9d45f8bfa391d
+echo "35" && git cherry-pick cea50fc3ec462f43a00b0ab0581f1b0d0c44fbc3
+echo "36" && git cherry-pick 61e301c5d88515f11816a93b9d56a493e20b06bd
+echo "37" && git cherry-pick 85b2f36c1b914f370fd8feb4e5a7f4074e7153f5
+echo "38" && git cherry-pick ddcbbbe80c869f4d907a4b6a937109f843e6bf43
+echo "39" && git cherry-pick f4d3332162431d470e8b2d06a9e7964dae930b5a
+echo "40" && git cherry-pick 837f9818fd0b9834d8e98cf0e172d29009d44eba
+echo "41" && git cherry-pick dc4b63c3bb8d30c904c9448502826ff019ea772b
+echo "42" && git cherry-pick 457952c19c304f08bdf74ed975164142704ee9b9
+echo "43" && git cherry-pick a5a6b824a034faba2943c3af54e1b31b281bad77
+echo "44" && git cherry-pick e2aa0e59e5a27aabea6293d985a2c6ba3e5b6625
+echo "45" && git cherry-pick 9029418b70203d265e90effd599cc0d05d1209d8
+echo "46" && git cherry-pick 95ca6e3640862aead1621cf3f367fc5f13adb62f
+echo "47" && git cherry-pick a81fcb52a4366d31f62b6c7f039694d75bb90e71
+echo "48" && git cherry-pick 3bb7c58060d22f37abad534b1235fb066d5620be
+echo "49" && git cherry-pick 307f50b4bc4d9569650d53a1fef966dc58e32051
+echo "50" && git cherry-pick d812651dbcae1fd2e2e38f0f94548e7bf9208c6f
+echo "51" && git cherry-pick 623173b85848f9289eb42f48009247f2152177f5
+echo "52" && git cherry-pick 4edb35d623c4d21945e13d6472d52499fc46e94b
+echo "53" && git cherry-pick 5e1fa4b38e12204b3e4175964ff4d65034db3846
+echo "54" && git cherry-pick fe9b6f69fe79b2a2c380099f395ad9286642dbbf
+echo "55" && git cherry-pick 6069b2c74d489eed540d85fdc43e3938f6bf6db0
+echo "56" && git cherry-pick a853724cdae236391614c59417a09570efa7cc76
+echo "57" && git cherry-pick a026f92e6f0162b5192eb92816ca2815afe2300c
+echo "58" && git cherry-pick 6b1d065addc03de59f0ababd8018733d3fb734d7
+echo "59" && git cherry-pick ef5d62faf1bd0a913c4cc7a66d1d11fde04d9f56
+echo "60" && git cherry-pick 18e6c6ee9815a8f97c7029b626902b79af58c1ac
+echo "61" && git cherry-pick 323cbb23efe094670f57ab020df9e0ad8dd9f898
+echo "62" && git cherry-pick 17de104878d6d3bded339ccc49c41e4ff39a4c3c
+echo "63" && git cherry-pick 95bc2205a852dd07072ab79afb7394e565f1107d
+echo "64" && git cherry-pick 1fd40ffba3e9daf97e942edb517f451bde74eb69
+echo "65" && git cherry-pick c070d154a01ca3721c39355ff102dbf26fc85e48
+echo "66" && git cherry-pick d519e5454b66f637c74a896ab55f1359ca3f5fb6
+echo "67" && git cherry-pick 139c280c2e8982b78bf9886188cd70bb320c8b90
+echo "68" && git cherry-pick 19c5e2f1cf7872eec053e6fecbd6e079f6895e92
+echo "69" && git cherry-pick 20b46e0bb9be1b96f3f5f1d00d2ae0166b0c949f
+echo "70" && git cherry-pick e6e8ef69f68aac248e2a83cab39e9db1dc1b4853 # (HEAD -> master, origin/master)
